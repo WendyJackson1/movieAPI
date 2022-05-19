6a6910bab5f8c4b8f45bc3a6dfb06aef7c34dafc
WendyJackson1 <wendyrobinh@gmail.com>
2022-05-19 09:19:28 -0400

    lab work

---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieAPI.Models
+{
+    public enum Genre
+    {
+        Horror,
+        Romance,
+        Comedy,
+        Animated
+
+    }
+    public class Movie
+    {
+        public string Title { get; set; }   
+        public int ReleaseYear { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+}
